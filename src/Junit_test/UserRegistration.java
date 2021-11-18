845df3e77f58a410b71f305479894b1dbe6681c9
PraveenkumarElanchezhian <epraveenkumar1997@gmail.com>
2021-11-18 19:21:50 +0530

    pre-define mobile number

--- src/Junit_test/UserRegistration.java
+++ src/Junit_test/UserRegistration.java
@@ -12,10 +12,13 @@ class Name {
 		String firstName = "Praveen";
 		String lastName="Kumar";
 		String email="abc.xyx@bl.co.in";
-		String regEx_Name = "^[A-Z]{1}[a-z]{2}[a-z]*";
+		String mobileNumber="91 9508865777";
+		String regEx_Name = "\\b([A-Z]\\w*)\\b";
 		String regEx_email = "^(.+)@(.+)$";
+		String regex_MobileNumber="[1-9]{2}[ \\S\\s]{1}+[1-9]*]";
 		System.out.println(firstName.matches(regEx_Name));
 		System.out.println(lastName.matches(regEx_Name));
 		System.out.println(email.matches(regEx_email));
+		System.out.println(mobileNumber.matches(regex_MobileNumber));
 	}
 }
