3258354117d5130114197e5d84e1f00707e365b8
PraveenkumarElanchezhian <epraveenkumar1997@gmail.com>
2021-11-18 19:07:08 +0530

    enter a valid Last Name

--- src/Junit_test/UserRegistration.java
+++ src/Junit_test/UserRegistration.java
@@ -2,15 +2,17 @@ package Junit_test;
 
 public class UserRegistration {
 	public static void main(String[] args) {
-		FirstName rv = new FirstName();
-		rv.firstName();
+		Name rv = new Name();
+		rv.first_LastName();
 	}
 }
 
-class FirstName {
-	public void firstName() {
+class Name {
+	public void first_LastName() {
 		String firstName = "Praveen";
+		String lastName="Kumar";
 		String regex = "^[A-Z]{1}[a-z]{2}[a-z]*";
 		System.out.println(firstName.matches(regex));
+		System.out.println(lastName.matches(regex));
 	}
 }
